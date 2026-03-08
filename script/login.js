aa977dba265acd7e1f64cb617508f613516cc00e
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-03-08 15:13:45 +0600

    again fixed path related issues

--- script/login.js
+++ script/login.js
@@ -3,7 +3,7 @@ const password = document.getElementById("password-input");
 
 document.getElementById("login-btn").addEventListener("click", () => {
     if(username.value === "admin" && password.value === "admin123") {
-        window.location.assign("main.html")
+        window.location.href = "main.html";
     }
     else{
         alert("Unable to sign in. Please try again.")
