9d87909a3ab188a8ecae313aad6249930e98aa5f
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-03-08 14:52:53 +0600

    added functionality to the login page and sign in button

--- script/login.js
+++ script/login.js
@@ -1,4 +1,11 @@
 const username = document.getElementById("username-input");
 const password = document.getElementById("password-input");
-const loginBtn = document.getElementById("login-btn");
 
+document.getElementById("login-btn").addEventListener("click", () => {
+    if(username.value === "admin" && password.value === "admin123") {
+        window.location.assign("/main.html")
+    }
+    else{
+        alert("Unable to sign in. Please try again.")
+    }
+});
