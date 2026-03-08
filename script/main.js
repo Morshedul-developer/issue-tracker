88941b1e5a33e4c7c5b8c59729de36921c6d6f58
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-03-08 20:35:40 +0600

    made the dynamics cards

--- script/main.js
+++ script/main.js
@@ -5,25 +5,25 @@ const loadIssues = async () => {
     const issues = await res.json();
     displayIssues(issues.data);
 }
-
+// #A855F7
+// #00A96E
 const displayIssues = (issues) => {
     issues.forEach(issue => {
         const div = document.createElement('div');
-        div.className = "space-y-3 p-4 rounded-lg shadow-md border-t-3 border-green-600";
+        div.className = `space-y-3 p-4 rounded-lg shadow-md border-t-3 border-[${issue.status == "open" ? "#00A96E" : "#A855F7"}]`;
         div.innerHTML = `
               <div class="space-y-3">
                 <div class="flex justify-between items-center">
-                  <img src="./assets/Open-Status.png" alt="" />
+                  <img src="./assets/${issue.status == "open" ? "Open-Status.png" : "Closed- Status .png"}" alt="" />
                   <div class="badge badge-soft badge-error rounded-2xl">
-                    HIGH
+                    ${issue.priority.toUpperCase()}
                   </div>
                 </div>
                 <h4 class="text-[14px] font-semibold">
-                  Fix navigation menu on mobile devices
+                  ${issue.title}
                 </h4>
-                <p class="text-[12px] text-gray-500">
-                  The navigation menu doesn't collapse properly on mobile
-                  devices...
+                <p class="text-[12px] text-gray-500 line-clamp-2">
+                  ${issue.description}
                 </p>
                 <div class="flex gap-1">
                   <div class="badge badge-soft badge-error border border-error rounded-2xl text-[12px]">
@@ -34,10 +34,16 @@ const displayIssues = (issues) => {
                   </div>
                 </div>
               </div>
-              <hr class="border-gray-200" />
+              <hr class="-mx-4 border-gray-200" />
               <div>
-                <p class="text-[12px] text-gray-500">#1 by john_doe</p>
-                <p class="text-[12px] text-gray-500">1/15/2024</p>
+                <div class="flex justify-between">
+                <p class="text-[12px] text-gray-500">#${issue.id} by ${issue.author}</p>
+                <span class="text-[12px] text-gray-500">${new Date(issue.createdAt).toLocaleDateString()}</span>
+                </div>
+                <div class="flex justify-between">
+                <p class="text-[12px] text-gray-500">Assignee: ${issue.assignee}</p>
+                <span class="text-[12px] text-gray-500">Updated: ${new Date(issue.updatedAt).toLocaleDateString()}</span>
+                </div>
               </div>
         `;
         issueContainer.append(div);
